d791dc61bc88fb0bd5d5212a7c3f420c7ba11411
Ujhafsuy <duardoescubemaya@gmail.com>
2025-02-26 12:00:29 -0300

    Estilização Header

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,12 @@ import moon from './assets/moon.svg'
 function App() {
 
   return (
-      <div>
-        <header>
-          <div>
-            <h1 className='text-red-300'>devfinder</h1>
-            <div>
-              <span>DARK</span>
+      <div id='container' className='bg-[#F6F8FF] h-dvh flex flex-col w-full items-center justify-center'>
+        <header className='w-[45.625rem]'>
+          <div className='flex justify-between w-full'>
+            <h1 className='text-[#2B3442] text-[1.625rem] font-bold'>devfinder</h1>
+            <div className='flex items-center gap-[1rem] mb-[2.25rem]'>
+              <span className='text-[#697C9A] text-[0.8125rem]'>DARK</span>
               <img src={moon} alt="Lua" />
             </div>
           </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import lupa from '../assets/lupa.png'
 
 function Search(){
     return(
         <div>
-            <div>
-            <img src="#" alt="search" />
-            <input type="text" name="" id="" />
-            <button>Search</button>
+            <div className='bg-[#FEFEFE] flex justify-between p-[1.375rem] rounded-[1rem]'>
+            <div className='flex items-center gap-[1.5rem]'>
+                <img src={lupa} alt="search" className='h-[1.5rem] w-[1.rem]'/>
+                <input type="text" name="" id=""  placeholder='Search Github username...' className='w-[30rem]'/>
+            </div>
+            <button className='bg-[#0079FF] text-[#FFF] pt-[0.75rem] pb-[0.75rem] pr-[1.5rem] pl-[1.5rem] rounded-[0.5rem]'>Search</button>
             </div>
         </div>
     )
