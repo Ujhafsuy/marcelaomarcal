7b9206c747bd095e2832a8401145b1868c6c0e75
Ujhafsuy <duardoescubemaya@gmail.com>
2025-03-17 10:14:44 -0300

    mobile finalizado

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -19,7 +19,7 @@ function Search({ onSearch, errado }){
         <div className='mb-[1.5rem] shadow-(--shadow) rounded-[1rem]'>
             <div className='bg-[var(--offwhite)] flex items-center justify-between p-[0.655rem] rounded-[1rem]'>
             <div className='flex items-center gap-[1.5rem] search'>
-                <img src={lupa} alt="search" className='h-[1.5rem] w-[1.rem] ml-[32px] lupa'/>
+                <img src={lupa} alt="search" className='h-[1.5rem] w-[1.5rem] ml-[32px] lupa'/>
                 <input onChange={(e) => setNome(e.target.value)} type="text" name="" id=""  placeholder='Search Github username...' className='w-[25rem] cursor-[pointer] placeholder-[var(--slateblue)] text-[var(--slateblue)]'/>
             </div>
                 {errado == true ? <span className='text-[var(--red-color)] font-bold text-[0.9375rem]'>No results</span> : <span className='text-[var(--red-color)] font-bold text-[0.9375rem] hidden'>No results</span>}
